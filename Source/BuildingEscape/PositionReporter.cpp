94467a122cdf04e20f8b79b0dcb99ef69ed1001b
dimitarbez <dimitarbez@gmail.com>
2018-06-14 13:51:24 +0200

    Video 63

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include <Gameframework/Actor.h>
 
 
 // Sets default values for this component's properties
@@ -18,8 +19,9 @@ UPositionReporter::UPositionReporter()
 void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
+	FString Objectname = GetOwner()->GetName();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty"));
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s"), *Objectname);
 	
 }
 
